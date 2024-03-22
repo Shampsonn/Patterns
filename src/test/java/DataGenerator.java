0cc07265a72251bfd6ecc979ac675cf087a16dc5
Alexander Bortsov <A.bortsovv@yandex.ru>
2024-03-22 18:35:26 +0300

    fixed

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -9,30 +9,42 @@ import java.util.Random;
 public class DataGenerator {
 
     public static String generateDate(int addDays, String pattern) {
-        return LocalDate.now() .plusDays(addDays). format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-    public static String generateCity(){
+
+    public static String generateCity() {
         String[] cities = new String[]{"Ярославль", "Якутск", "Москва", "Смоленск", "Челябинск", "Чебоксанры", "Уфа",
                 "Брянск", "Кызыл", "Вологда", "Мурманск", "Тула", "Липецк", "Омск", "Рязань", "Тюмень"};
         return cities[new Random().nextInt(cities.length)];
     }
 
-    public static String generateName(String locale){
+    public static String generateName(String locale) {
         Faker faker = new Faker(new Locale(locale));
         return faker.name().fullName();
 
     }
 
-    public static String generatePhone(String locale){
+    public static String generatePhone(String locale) {
         Faker faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
-    @Value
-    public static class GenerateUser{
-        String city = DataGenerator.generateCity();
-        String name = DataGenerator.generateName("ru");
-        String phone = DataGenerator.generatePhone("ru");
 
+    public static class Reg {
+        private Reg() {
+        }
+
+
+        public static GenerateUser person(String locale) {
+            return new GenerateUser(generateCity(), generateName("ru"), generatePhone("ru"));
+        }
     }
 
+    @Value
+    public static class GenerateUser {
+        String city;
+        String name;
+        String phone;
+
+    }
 }
+
--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTest {
@@ -18,23 +19,29 @@ public class DeliveryTest {
 
     @Test
     void ShouldBeDeliverySuccess() {
-        DataGenerator.GenerateUser user = new DataGenerator.GenerateUser();
+        DataGenerator.GenerateUser user = DataGenerator.Reg.person("ru");
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue(user.city);
+        $("[data-test-id='city'] input").setValue(user.getCity());
         String planningDate = DataGenerator.generateDate(5, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
-        $("[data-test-id='name'] input").setValue(user.name);
-        $("[data-test-id='phone'] input").setValue(user.phone);
+        $("[data-test-id='name'] input").setValue(user.getName());
+        $("[data-test-id='phone'] input").setValue(user.getPhone());
         $("[data-test-id='agreement']").click();
         $("button.button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + planningDate));
         planningDate = DataGenerator.generateDate(10, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("button.button").click();
-        $$(".button__content").find(Condition.exactText("Перепланировать")).click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldBe(Condition.visible)
                 .shouldHave(Condition.exactText("Встреча успешно запланирована на " + planningDate));
 
 
